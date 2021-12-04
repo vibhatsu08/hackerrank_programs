76b759f6b6cfbcf9aa19ac3b79ccda62fe3f318f
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-12-05 01:19:13 +0530

    Add files via upload
    Input Format
    You are given two strings,  and , separated by a new line. Each string will consist of lower case Latin characters ('a'-'z').
    Output Format
    In the first line print two space-separated integers, representing the length of and  respectively. 
    In the second line print the string produced by concatenating  and  (). 
    In the third line print two strings separated by a space,  and .  and  are the same as  and , respectively, except that their first characters are swapped.
    Sample Input
    abcd
    ef
    Sample Output
    4 2
    abcdef
    ebcd af
    Explanation
     "abcd"
     "ef"
    
    
     "abcdef"
     "ebcd"
     "af"

---
+++ hackerrankStringProgram/hackerrankStringProgram/main.cpp
@@ -0,0 +1,45 @@
+
+/*Input Format
+ You are given two strings,  and , separated by a new line. Each string will consist of lower case Latin characters ('a'-'z').
+ Output Format
+ In the first line print two space-separated integers, representing the length of and  respectively.
+ In the second line print the string produced by concatenating  and  ().
+ In the third line print two strings separated by a space,  and .  and  are the same as  and , respectively, except that their first characters are swapped.
+ Sample Input
+ abcd
+ ef
+ Sample Output
+ 4 2
+ abcdef
+ ebcd af
+ Explanation
+  "abcd"
+  "ef"
+
+
+  "abcdef"
+  "ebcd"
+  "af"*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    //this is the input
+    getline(cin, a);
+    getline(cin, b);
+    
+    //this is the output
+    cout << a.size() << " " << b.size() << endl;
+    
+    cout << a << b << endl;
+    char ch = b[0];
+    b[0] = a[0];
+    a[0] = ch;
+    
+    cout << a << " " << b << endl;
+    
+    
+    return 0;
+}
